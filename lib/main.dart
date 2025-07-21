05f1d15c1b9c59d3b4ab3a8cae56f771db29619a
shivam kanathe <shivamkanathe@gmail.com>
2025-07-21 01:37:32 -0700

    create new release

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ void main()async{
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  @override
+  @override  
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
