4918c6fc57027e0147afa8255c0a8506a76855a2
shivam kanathe <shivamkanathe@gmail.com>
2025-07-21 02:14:12 -0700

    create release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.communicate"
     compileSdk = 35
-    ndkVersion = 27
+    ndkVersion = 27.0.11718014
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
